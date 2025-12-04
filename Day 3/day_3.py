5fca29a19f0545e99937b9435a697cd4f7c6787a
Severag <odonobii@gmail.com>
2025-12-03 18:16:56 -0600

    Solve Day 3

--- Day 3/day_3.py
+++ Day 3/day_3.py
@@ -1,14 +1,10 @@
-import collections, heapq, itertools, re
-import numpy as np
+
 
 
 def read_file(filename):
-    num_parse = lambda line: list(map(int, line.strip().split()))
-    str_parse = lambda line: line.strip().split()
-    regex = r""
-    regex_parse = lambda line: re.findall(regex, line)[0]
+    num_parse = lambda line: list(map(int, line.strip()))
     
-    with open( filename, 'r') as f:
+    with open(filename, 'r') as f:
         data = list(map(num_parse, f))
     
     return data
@@ -24,17 +20,50 @@ def solve(data, do_1=True, do_2=True):
 
 
 def part1(data):
-    return
+    total = 0
+    
+    for line in data:
+        first,idx = get_next_digit(line, 1)
+        second,_ = get_next_digit(line[idx+1:], 0)
+        
+        joltage = 10 * first + second
+        total += joltage
+
+    return total
+
+
+
+def get_next_digit(array, with_remaining):
+    end_idx = -abs(with_remaining) if with_remaining !=0 else None
+    
+    num = max(array[:end_idx])
+    idx = array.index(num)
+    
+    return num, idx
 
 
 
 def part2(data):
-    return
+    total = 0
+    
+    for line in data:
+        joltage = 0
+        idx = 0
+        
+        for end_buffer in reversed(range(12)):
+            new_digit, d_idx = get_next_digit(line[idx:], end_buffer)
+            
+            joltage = 10 * joltage + new_digit
+            idx += d_idx + 1
+        
+        total += joltage 
+    
+    return total
 
 
 
 if __name__ == '__main__':
-    puzzles = [['test_case.txt', None, None],
+    puzzles = [['test_case.txt', 357, 3_121_910_778_619],
                ['puzzle_input.txt', ]]
     
     try:
