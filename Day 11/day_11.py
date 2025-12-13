2a957f77a5b82efa13100ace244b90cf1d5f1a3e
Severag <odonobii@gmail.com>
2025-12-12 18:16:25 -0600

    Solve Day 11

--- Day 11/day_11.py
+++ Day 11/day_11.py
@@ -1,15 +1,17 @@
-import collections, heapq, itertools, re
+import collections
 import numpy as np
 
 
 def read_file(filename):
-    num_parse = lambda line: list(map(int, line.strip().split()))
-    str_parse = lambda line: line.strip().split()
-    regex = r""
-    regex_parse = lambda line: re.findall(regex, line)[0]
+    def parse(line):
+        key, remainder = line.strip().split(': ')
+        
+        return key, remainder.split()
     
     with open( filename, 'r') as f:
-        data = list(map(num_parse, f))
+        data = dict(map(parse, f))
+    
+    data['out'] = []
     
     return data
 
@@ -24,17 +26,112 @@ def solve(data, do_1=True, do_2=True):
 
 
 def part1(data):
-    return
+    start = 'you'
+    end = 'out'
+    
+    open_list = [[start]]
+    completed_count = 0
+    
+    while open_list:
+        path = open_list.pop()
+        curr = path[-1]
+        
+        if curr == end:
+            completed_count += 1
+        elif curr in data:
+            for cand in data[curr]:
+                open_list.append(path + [cand])
+    
+    return completed_count
 
 
 
 def part2(data):
-    return
+    total = 0
+    topo_order = topo_sort(data, 'svr')
+    rev_graph = reverse_graph(data)
+    
+    all_routes = ['svr fft dac out'.split(),
+                  'svr dac fft out'.split()]
+    
+    for route in all_routes:
+        num_paths = 1
+        for start,end in zip(route[:-1], route[1:]):
+            if end == 'fft':
+                avoid = 'dac'
+            elif end == 'dac':
+                avoid = 'fft'
+            else:
+                avoid = None
+            num_paths *= count_paths(rev_graph, topo_order, start, end, avoid)
+        
+        total += num_paths
+    
+    return total
+
+
+
+def reverse_graph(graph):
+    rev = collections.defaultdict(list)
+    
+    for key, items in graph.items():
+        for val in items:
+            rev[val].append(key)
+    
+    return rev
+
+
+
+def topo_sort(data, start):
+    # modified from https://stackoverflow.com/questions/47192626/deceptively-simple-implementation-of-topological-sorting-in-python
+    open_list = [start]
+    stack = []
+    order = []
+    visited = set()
+    
+    while open_list:
+        curr = open_list.pop()
+        
+        if curr in visited:
+            continue
+        visited.add(curr)
+        open_list.extend(data[curr])
+        
+        # traverse back through unexplored branches
+        while stack and curr not in data[stack[-1]]:
+            order.append(stack.pop())
+        
+        stack.append(curr)
+    
+    return stack + order[::-1]
+
+
+
+def count_paths(rev_graph, topo, start, end, avoid):
+    path_counts = collections.defaultdict(int)
+    path_counts[start] = 1
+    
+    begin = topo.index(start) + 1
+    
+    for node in topo[begin:]:
+        if node == avoid:
+            continue
+        num_paths = 0
+        for parent in rev_graph[node]:
+            num_paths += path_counts[parent]
+        
+        path_counts[node] = num_paths
+        
+        if node == end:
+            break
+    
+    return path_counts[end]
 
 
 
 if __name__ == '__main__':
-    puzzles = [['test_case.txt', None, None],
+    puzzles = [['test_case.txt', 5, None],
+               ['test_case_2.txt', None, 2],
                ['puzzle_input.txt', ]]
     
     try:
